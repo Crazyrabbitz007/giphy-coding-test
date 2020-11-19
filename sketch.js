74d343050ba3bccd6eeb5bb8946d2e86038bf207
Crazyrabbitz007 <61986489+Crazyrabbitz007@users.noreply.github.com>
2020-11-19 14:51:20 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,18 @@
-var api = "https://api.giphy.com/v1/gifs/search?"
-var apiKey = "&api_key=STIIwOZ3IsnkOC3VzyaVRRoDV7CaDKOG";
-var query = "&q=heat";
-var url = api + apiKey + query;
+var api = "https://api.giphy.com/v1/gifs/search?" // path to access data
+var apiKey = "&api_key=STIIwOZ3IsnkOC3VzyaVRRoDV7CaDKOG"; //key to allow access
+var query = "&q=coding"; //text for search
+var url = api + apiKey + query; //combines elements into a single URL
 
 function setup() {
   noCanvas();
-  loadJSON(url, gotData);
+  loadJSON(url, gotData); //loads url and then displays them on screen
 }
 
 function gotData(giphy) {
   for (var i = 0; i < giphy.data.length; i++) createImg(giphy.data[i].images.original.url,alt=query);
+    //array to display multiple gifs under the search query
 }
 
 function draw() {
-  background(220);
+  background(255); // a plain white background :)
 }
